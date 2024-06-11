f497261378951cb22031da4151511e8e8e440315
Dennis Wanjiru <denniswanjiru71@gmail.com>
2024-06-11 12:47:57 +0300

    create reading list page

---
+++ frontend/src/pages/reading-list/index.tsx
@@ -0,0 +1,14 @@
+import { Grid } from "@mui/material";
+import BookCard from "../../components/book-card";
+
+export default function ReadingList() {
+  return (
+    <Grid container spacing={6}>
+      {[1, 2, 3, 4, 5].map((idx) => (
+        <Grid key={idx} item xs={12} md={6} lg={4}>
+          <BookCard />
+        </Grid>
+      ))}
+    </Grid>
+  );
+}
