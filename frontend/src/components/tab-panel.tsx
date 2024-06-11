f1e29dafb7b6a09e1e49682be4f128ba050d3997
Dennis Wanjiru <denniswanjiru71@gmail.com>
2024-06-11 12:46:13 +0300

    create tabs to switch between all books and reading list

---
+++ frontend/src/components/tab-panel.tsx
@@ -0,0 +1,28 @@
+import { Box } from "@mui/material";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+const TabPanel: React.FC<TabPanelProps> = ({
+  children,
+  value,
+  index,
+  ...other
+}) => {
+  return (
+    <Box
+      role="tabpanel"
+      hidden={value !== index}
+      id={`tabpanel-${index}`}
+      aria-labelledby={`tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box py={5}>{children}</Box>}
+    </Box>
+  );
+};
+
+export default TabPanel;
