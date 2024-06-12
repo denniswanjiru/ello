fb98494a4e7cb40cca4481b586e4c11d23242376
Dennis Wanjiru <denniswanjiru71@gmail.com>
2024-06-12 06:13:52 +0300

    add empty state for the reading list

--- frontend/src/pages/reading-list/index.tsx
+++ frontend/src/pages/reading-list/index.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@apollo/client";
-import { Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import BookCard from "../../components/book-card";
 import { Book } from "../../generated/graphql";
 import { GET_READING_LIST } from "../../graphql/queries";
@@ -12,6 +12,22 @@ export default function ReadingList() {
   if (!error && !data) return <div>Loading</div>;
 
   if (error) return <div>Something went wrong</div>;
+
+  if (!data.readingList.length) {
+    return (
+      <Box display="grid" sx={{ placeItems: "center", mt: 14 }}>
+        <img src="/assets/empty.png" alt="empty reading list" />
+
+        <Typography variant="h5" fontWeight="bold" color="#919EAB">
+          No books to display
+        </Typography>
+
+        <Typography color="#919EAB">
+          Oops! Seems like there are no books in the reading list.
+        </Typography>
+      </Box>
+    );
+  }
 
   return (
     <Grid container spacing={6}>
